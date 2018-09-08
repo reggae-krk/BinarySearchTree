32365a4955a7a601e680332015b64355273feeb7
reggae-krk <monika.gorecka.agh@gmail.com>
2018-09-08 14:05:57 +0200

    Create constructor and createBinaryTreeFromArray method

--- src/main/java/BinarySearchTree.java
+++ src/main/java/BinarySearchTree.java
@@ -1,2 +1,30 @@
 public class BinarySearchTree {
+
+    private Node root;
+
+    public BinarySearchTree(int[] array) {
+
+        int indexRootData = array.length/2;
+        int rootData = array[indexRootData];
+        this.root = new Node(rootData);
+        createBinaryTreeFromArray(array, 0, indexRootData);
+        createBinaryTreeFromArray(array, indexRootData + 1, array.length);
+    }
+
+    private void createBinaryTreeFromArray(int[] array, int startIndex, int endIndex) {
+
+        Node tempRoot = this.root;
+
+        for (int i = startIndex; i < endIndex; i++) {
+
+            if (array[i] < tempRoot.getData()) {
+                tempRoot.setLeft(new Node(array[i]));
+                tempRoot = tempRoot.getLeft();
+            }
+            else {
+                tempRoot.setRight(new Node(array[i]));
+                tempRoot = tempRoot.getRight();
+            }
+        }
+    }
 }
